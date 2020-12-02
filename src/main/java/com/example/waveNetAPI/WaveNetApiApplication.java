bb7f8f5edc7ce996dfd9af143ef8401e0ae6fe13
VimukthiGunarathna <vimukthiappleid@gmail.com>
2020-12-02 09:31:07 +0530

    initial commit : Basic project structure

---
+++ src/main/java/com/example/waveNetAPI/WaveNetApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.waveNetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveNetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveNetApiApplication.class, args);
+	}
+
+}
