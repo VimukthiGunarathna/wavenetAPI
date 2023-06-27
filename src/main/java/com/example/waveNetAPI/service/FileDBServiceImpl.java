3054b06ff55a3ec52e5c7f8ee121b86b7f05d530
Vimukthi Gunarathna <vimukthi@globalwavenet.com>
2023-06-27 17:21:00 +0530

    fix compilation error

--- src/main/java/com/example/waveNetAPI/service/FileDBServiceImpl.java
+++ src/main/java/com/example/waveNetAPI/service/FileDBServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.stream.Stream;
 
 public class FileDBServiceImpl implements FileDBService {
@@ -16,7 +17,12 @@ public class FileDBServiceImpl implements FileDBService {
     @Override
     public FileDB store(MultipartFile file) {
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+        FileDB FileDB = null;
+        try {
+            FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
         return fileDBDao.save(FileDB);
     }
